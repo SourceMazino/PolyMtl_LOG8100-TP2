f16838d8ef20488b81ac158ee504cbe0651c41b9
Kevin <Mazin0Code@gmail.com>
2024-10-03 16:27:24 -0400

    Transition vers postgressql

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 module.exports = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST || 'mysql-db',
-  port: process.env.MYSQL_PORT || 3306,
-  dialect: 'mysql'
-}
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: process.env.POSTGRES_PORT || 5432,
+  dialect: 'postgres'
+};
+
